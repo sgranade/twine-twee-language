084081ba37699b714121f0ea7bfc871cc6b7edd0
Stephen Granade <stephen@granades.com>
2024-10-14 18:38:18 -0500

    Add SugarCube TwineScript tokenizer.

---
+++ server/src/passage-text-parsers/sugarcube/sc2/sc2-twinescript.ts
@@ -0,0 +1,236 @@
+import { TextDocument } from "vscode-languageserver-textdocument";
+
+import { JSPropertyLabel, tokenizeJSExpression } from "../../../js-parser";
+import { createLocationFor } from "../../../parser";
+import { Label } from "../../../project-index";
+import { capturePreTokenFor, StoryFormatParsingState } from "../..";
+
+/**
+ * Mapping of TwineScript sugaring to replacements.
+ *
+ * Adapted from SugarCube's `scripting.js`
+ */
+const desugarMap: { [key: string]: string } = {
+    // Story $variable sigil-prefix.
+    $: " ",
+    // Temporary _variable sigil-prefix.
+    _: " ",
+    // Assignment operator.
+    to: "=",
+    // Equality operators.
+    eq: "==",
+    neq: "!=",
+    is: "===",
+    isnot: "!==",
+    // Relational operators.
+    gt: ">",
+    gte: ">=",
+    lt: "<",
+    lte: "<=",
+    // Logical operators.
+    and: "&&",
+    or: "||",
+    // Unary operators.
+    not: "!",
+    // `def` and `ndef` expand to multi-element expressions like `"undefined" !== typeof`.
+    // Since we're only desugaring TwineScript to create semantic tokens, we'll fake the
+    // funk and swap them out with a single unary operator so we get the right semantic token
+    def: "!",
+    ndef: "!",
+};
+/**
+ * RegExp to find keys in the desugarMap
+ */
+const desugarRegExp = new RegExp(
+    [
+        "[$_](?=[A-Za-z$_])", // Variable sigils
+        "\\b(?:to|n?eq|is(?:not)?|gte?|lte?|and|or|not|n?def)\\b", // Word operators
+    ].join("|"),
+    "g"
+);
+
+/**
+ * Position mapping for position-preserving substitution.
+ */
+interface PositionMapping {
+    originalStart: number;
+    originalString: string; // Save the original string for undoing the substitution
+    newStart: number;
+    newEnd: number;
+}
+
+interface DesugarResult {
+    desugared: string;
+    positionMapping: PositionMapping[];
+}
+
+/**
+ * Replace TwineScript syntactic sugar with its Javascript equivalents.
+ *
+ * Note that `def` and `ndef` aren't properly translated to
+ * `"undefined" !== typeof` and `"undefined" === typeof`
+ *
+ * @param str TwineScript string to desugar.
+ * @returns The desugared string and mapping of old to new positions.
+ */
+export function desugar(str: string): DesugarResult {
+    const result: DesugarResult = {
+        desugared: "",
+        positionMapping: [],
+    };
+
+    // Replace all sugar constructs with their JavaScript equivalent
+    // using position-preserving substitution.
+    let sliceStart = 0;
+    let runningDelta = 0;
+    desugarRegExp.lastIndex = 0;
+    for (const m of str.matchAll(desugarRegExp)) {
+        const replacement = desugarMap[m[0]] || "";
+        result.desugared += str.slice(sliceStart, m.index) + replacement;
+        result.positionMapping.push({
+            originalStart: m.index,
+            originalString: m[0],
+            newStart: m.index + runningDelta,
+            newEnd: m.index + runningDelta + replacement.length,
+        });
+        runningDelta += replacement.length - m[0].length;
+        sliceStart = m.index + m[0].length;
+    }
+    result.desugared += str.slice(sliceStart);
+
+    return result;
+}
+
+interface SugaredPositionAndText {
+    /**
+     * Position in a sugared string.
+     */
+    sugaredPosition: number;
+    /**
+     * The original sugared text at the desugared position, if any.
+     */
+    sugaredText?: string;
+}
+
+/**
+ * Get the delta from desugared to sugared TwineScript string and original sugared text (if any).
+ *
+ * @param desugaredPosition Position in the desugared string.
+ * @param positionMapping Position mapping from desugaring the TwineScript string.
+ * @returns Delta from desugared to sugared position
+ */
+function getSugaredPositionAndNewText(
+    desugaredPosition: number,
+    positionMapping: PositionMapping[]
+): SugaredPositionAndText {
+    const ret: SugaredPositionAndText = {
+        sugaredPosition: desugaredPosition,
+    };
+
+    // Find the position mapping right before the desugared position
+    let ndx = positionMapping.findIndex((p) => desugaredPosition < p.newStart);
+    if (ndx === -1) ndx = positionMapping.length;
+    // Adjustments are only needed after the first position mapping
+    if (ndx !== 0) {
+        // The mapping we want is the one before the index
+        const mapping = positionMapping[ndx - 1];
+
+        // If the offsets match, then we're at a desugared value and need to
+        // swap in the original sugared value
+        if (desugaredPosition === mapping.newStart) {
+            ret.sugaredText = mapping.originalString;
+            ret.sugaredPosition += mapping.originalStart - mapping.newStart;
+        } else {
+            ret.sugaredPosition +=
+                mapping.originalStart +
+                mapping.originalString.length -
+                mapping.newEnd;
+        }
+    }
+
+    return ret;
+}
+
+/**
+ * Tokenize a TwineScript expression and find referenced variables and properties in it.
+ *
+ * Returned properties are only those for which the parser could trace their "ownership"
+ * back to a root variable.
+ *
+ * @param expression Expression to parse.
+ * @param offset Offset into the document where the expression occurs.
+ * @param state Parsing state.
+ * @param storyFormatState Passage state object that will collect tokens.
+ * @returns Two-tuple with separate lists of variable and property labels found in parsing.
+ */
+export function tokenizeTwineScriptExpression(
+    expression: string,
+    offset: number,
+    textDocument: TextDocument,
+    storyFormatState: StoryFormatParsingState
+): [Label[], JSPropertyLabel[]] {
+    const { desugared, positionMapping } = desugar(expression);
+
+    // Turning TwineScript into JavaScript changes positions within a string.
+    // We have to translate the positions of Javascript variable and property labels,
+    // as well as semantic tokens, into positions relative to the original TwineScript
+    // expression. As part of that, we'll stash the JavaScript semantic tokens in
+    // a temporary story format parsing state.
+    const desugaredStoryFormatState: StoryFormatParsingState = {
+        passageTokens: {},
+    };
+    // We'll also create a fake document that contains just the expression
+    const desugaredDocument = TextDocument.create(
+        "fake-uri",
+        "javascript",
+        1,
+        desugared
+    );
+
+    const [vars, props] = tokenizeJSExpression(
+        desugared,
+        0, // offset of 0 since the desugared expression starts at the start of the doc
+        desugaredDocument,
+        desugaredStoryFormatState
+    );
+
+    // Adjust variable and property locations and (if needed) text
+    for (const v of [...vars, ...props]) {
+        const desugaredOffset = desugaredDocument.offsetAt(
+            v.location.range.start
+        );
+
+        const { sugaredPosition, sugaredText } = getSugaredPositionAndNewText(
+            desugaredOffset,
+            positionMapping
+        );
+
+        if (sugaredText !== undefined) v.contents = sugaredText;
+
+        v.location = createLocationFor(
+            v.contents,
+            offset + sugaredPosition,
+            textDocument
+        );
+    }
+
+    // Adjust semantic token locations and (if needed) text
+    for (const t of Object.values(desugaredStoryFormatState.passageTokens)) {
+        const { sugaredPosition, sugaredText } = getSugaredPositionAndNewText(
+            t.at,
+            positionMapping
+        );
+
+        if (sugaredText !== undefined) t.text = sugaredText;
+
+        capturePreTokenFor(
+            t.text,
+            offset + sugaredPosition,
+            t.type,
+            t.modifiers,
+            storyFormatState
+        );
+    }
+
+    return [vars, props];
+}
---
+++ server/src/test/passage-text-parsers/sugarcube/sc2/sc2-twinescript.test.ts
@@ -0,0 +1,577 @@
+import "mocha";
+import { expect } from "chai";
+import { Location, Range } from "vscode-languageserver";
+import { TextDocument } from "vscode-languageserver-textdocument";
+
+import { StoryFormatParsingState } from "../../../../passage-text-parsers";
+import { ETokenModifier, ETokenType } from "../../../../tokens";
+import * as uut from "../../../../passage-text-parsers/sugarcube/sc2/sc2-twinescript";
+
+describe("SugarCube TwineScript", () => {
+    describe("desugar", () => {
+        it("should desugar variable sigils", () => {
+            // Arrange
+
+            const result = uut.desugar("First $permanent then _temp");
+
+            expect(result.desugared).to.eql("First  permanent then  temp");
+            expect(result.positionMapping).to.eql([
+                {
+                    originalStart: 6,
+                    originalString: "$",
+                    newStart: 6,
+                    newEnd: 7,
+                },
+                {
+                    originalStart: 22,
+                    originalString: "_",
+                    newStart: 22,
+                    newEnd: 23,
+                },
+            ]);
+        });
+
+        it("should desugar the assignment operator", () => {
+            // Arrange
+
+            const result = uut.desugar("$var to 17");
+
+            expect(result.desugared).to.eql(" var = 17");
+            expect(result.positionMapping).to.eql([
+                {
+                    originalStart: 0,
+                    originalString: "$",
+                    newStart: 0,
+                    newEnd: 1,
+                },
+                {
+                    originalStart: 5,
+                    originalString: "to",
+                    newStart: 5,
+                    newEnd: 6,
+                },
+            ]);
+        });
+
+        it("should desugar equality operators", () => {
+            // Arrange
+
+            const result = uut.desugar("(((1 eq 2) neq 3) is 4) isnot 5");
+
+            expect(result.desugared).to.eql("(((1 == 2) != 3) === 4) !== 5");
+            expect(result.positionMapping).to.eql([
+                {
+                    originalStart: 5,
+                    originalString: "eq",
+                    newStart: 5,
+                    newEnd: 7,
+                },
+                {
+                    originalStart: 11,
+                    originalString: "neq",
+                    newStart: 11,
+                    newEnd: 13,
+                },
+                {
+                    originalStart: 18,
+                    originalString: "is",
+                    newStart: 17,
+                    newEnd: 20,
+                },
+                {
+                    originalStart: 24,
+                    originalString: "isnot",
+                    newStart: 24,
+                    newEnd: 27,
+                },
+            ]);
+        });
+
+        it("should desugar relational operators", () => {
+            // Arrange
+
+            const result = uut.desugar("(((1 gt 2) gte 3) lt 4) lte 5");
+
+            expect(result.desugared).to.eql("(((1 > 2) >= 3) < 4) <= 5");
+            expect(result.positionMapping).to.eql([
+                {
+                    originalStart: 5,
+                    originalString: "gt",
+                    newStart: 5,
+                    newEnd: 6,
+                },
+                {
+                    originalStart: 11,
+                    originalString: "gte",
+                    newStart: 10,
+                    newEnd: 12,
+                },
+                {
+                    originalStart: 18,
+                    originalString: "lt",
+                    newStart: 16,
+                    newEnd: 17,
+                },
+                {
+                    originalStart: 24,
+                    originalString: "lte",
+                    newStart: 21,
+                    newEnd: 23,
+                },
+            ]);
+        });
+
+        it("should desugar logical operators", () => {
+            // Arrange
+
+            const result = uut.desugar("(true or false) and true");
+
+            expect(result.desugared).to.eql("(true || false) && true");
+            expect(result.positionMapping).to.eql([
+                {
+                    originalStart: 6,
+                    originalString: "or",
+                    newStart: 6,
+                    newEnd: 8,
+                },
+                {
+                    originalStart: 16,
+                    originalString: "and",
+                    newStart: 16,
+                    newEnd: 18,
+                },
+            ]);
+        });
+
+        it("should desugar unary operators", () => {
+            // Arrange
+
+            const result = uut.desugar("not false && def var1 && ndef var2");
+
+            expect(result.desugared).to.eql("! false && ! var1 && ! var2");
+            expect(result.positionMapping).to.eql([
+                {
+                    originalStart: 0,
+                    originalString: "not",
+                    newStart: 0,
+                    newEnd: 1,
+                },
+                {
+                    originalStart: 13,
+                    originalString: "def",
+                    newStart: 11,
+                    newEnd: 12,
+                },
+                {
+                    originalStart: 25,
+                    originalString: "ndef",
+                    newStart: 21,
+                    newEnd: 22,
+                },
+            ]);
+        });
+    });
+
+    describe("tokenize TwineScript", () => {
+        it("should set semantic tokens for variable assignment", () => {
+            const expression = "$varbl to 'testy'";
+            const offset = 13;
+            const textDocument = TextDocument.create(
+                "fake-uri",
+                "twine",
+                1,
+                `twinescript: ${expression}`
+            );
+            const storyState: StoryFormatParsingState = {
+                passageTokens: {},
+            };
+
+            uut.tokenizeTwineScriptExpression(
+                expression,
+                offset,
+                textDocument,
+                storyState
+            );
+            const result = storyState.passageTokens;
+
+            expect(result).to.eql({
+                14: {
+                    text: "varbl",
+                    at: 14,
+                    type: ETokenType.variable,
+                    modifiers: [],
+                },
+                20: {
+                    text: "to",
+                    at: 20,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                23: {
+                    text: "'testy'",
+                    at: 23,
+                    type: ETokenType.string,
+                    modifiers: [],
+                },
+            });
+        });
+
+        it("should return apparent variables for variable assignment", () => {
+            const expression = "$varbl to 'testy'";
+            const offset = 13;
+            const textDocument = TextDocument.create(
+                "fake-uri",
+                "twine",
+                1,
+                `twinescript:\n${expression}`
+            );
+            const storyState: StoryFormatParsingState = {
+                passageTokens: {},
+            };
+
+            const result = uut.tokenizeTwineScriptExpression(
+                expression,
+                offset,
+                textDocument,
+                storyState
+            );
+
+            expect(result[0]).to.eql([
+                {
+                    contents: "varbl",
+                    location: Location.create(
+                        "fake-uri",
+                        Range.create(1, 1, 1, 6)
+                    ),
+                },
+            ]);
+            expect(result[1]).to.be.empty;
+        });
+
+        it("should return apparent properties for variable assignment", () => {
+            const expression = "$varbl.rootprop1.rootprop2 to 'testy'";
+            const offset = 13;
+            const textDocument = TextDocument.create(
+                "fake-uri",
+                "twine",
+                1,
+                `twinescript:\n${expression}`
+            );
+            const storyState: StoryFormatParsingState = {
+                passageTokens: {},
+            };
+
+            const result = uut.tokenizeTwineScriptExpression(
+                expression,
+                offset,
+                textDocument,
+                storyState
+            );
+
+            expect(result[1]).to.eql([
+                {
+                    contents: "rootprop1",
+                    location: Location.create(
+                        "fake-uri",
+                        Range.create(1, 7, 1, 16)
+                    ),
+                    scope: "varbl",
+                },
+                {
+                    contents: "rootprop2",
+                    location: Location.create(
+                        "fake-uri",
+                        Range.create(1, 17, 1, 26)
+                    ),
+                    scope: "varbl.rootprop1",
+                },
+            ]);
+        });
+
+        it("should set semantic tokens for equality operators", () => {
+            const expression = "(((1 eq 2) neq 3) is 4) isnot 5";
+            const offset = 13;
+            const textDocument = TextDocument.create(
+                "fake-uri",
+                "twine",
+                1,
+                `twinescript: ${expression}`
+            );
+            const storyState: StoryFormatParsingState = {
+                passageTokens: {},
+            };
+
+            uut.tokenizeTwineScriptExpression(
+                expression,
+                offset,
+                textDocument,
+                storyState
+            );
+            const result = storyState.passageTokens;
+
+            expect(result).to.eql({
+                16: {
+                    text: "1",
+                    at: 16,
+                    type: ETokenType.number,
+                    modifiers: [],
+                },
+                18: {
+                    text: "eq",
+                    at: 18,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                21: {
+                    text: "2",
+                    at: 21,
+                    type: ETokenType.number,
+                    modifiers: [],
+                },
+                24: {
+                    text: "neq",
+                    at: 24,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                28: {
+                    text: "3",
+                    at: 28,
+                    type: ETokenType.number,
+                    modifiers: [],
+                },
+                31: {
+                    text: "is",
+                    at: 31,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                34: {
+                    text: "4",
+                    at: 34,
+                    type: ETokenType.number,
+                    modifiers: [],
+                },
+                37: {
+                    text: "isnot",
+                    at: 37,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                43: {
+                    text: "5",
+                    at: 43,
+                    type: ETokenType.number,
+                    modifiers: [],
+                },
+            });
+        });
+
+        it("should set semantic tokens for relational operators", () => {
+            const expression = "(((1 gt 2) gte 3) lt 4) lte 5";
+            const offset = 13;
+            const textDocument = TextDocument.create(
+                "fake-uri",
+                "twine",
+                1,
+                `twinescript: ${expression}`
+            );
+            const storyState: StoryFormatParsingState = {
+                passageTokens: {},
+            };
+
+            uut.tokenizeTwineScriptExpression(
+                expression,
+                offset,
+                textDocument,
+                storyState
+            );
+            const result = storyState.passageTokens;
+
+            expect(result).to.eql({
+                16: {
+                    text: "1",
+                    at: 16,
+                    type: ETokenType.number,
+                    modifiers: [],
+                },
+                18: {
+                    text: "gt",
+                    at: 18,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                21: {
+                    text: "2",
+                    at: 21,
+                    type: ETokenType.number,
+                    modifiers: [],
+                },
+                24: {
+                    text: "gte",
+                    at: 24,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                28: {
+                    text: "3",
+                    at: 28,
+                    type: ETokenType.number,
+                    modifiers: [],
+                },
+                31: {
+                    text: "lt",
+                    at: 31,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                34: {
+                    text: "4",
+                    at: 34,
+                    type: ETokenType.number,
+                    modifiers: [],
+                },
+                37: {
+                    text: "lte",
+                    at: 37,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                41: {
+                    text: "5",
+                    at: 41,
+                    type: ETokenType.number,
+                    modifiers: [],
+                },
+            });
+        });
+
+        it("should set semantic tokens for logical operators", () => {
+            const expression = "(_varbl or false) and true";
+            const offset = 13;
+            const textDocument = TextDocument.create(
+                "fake-uri",
+                "twine",
+                1,
+                `twinescript: ${expression}`
+            );
+            const storyState: StoryFormatParsingState = {
+                passageTokens: {},
+            };
+
+            uut.tokenizeTwineScriptExpression(
+                expression,
+                offset,
+                textDocument,
+                storyState
+            );
+            const result = storyState.passageTokens;
+
+            expect(result).to.eql({
+                15: {
+                    text: "varbl",
+                    at: 15,
+                    type: ETokenType.variable,
+                    modifiers: [],
+                },
+                21: {
+                    text: "or",
+                    at: 21,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                24: {
+                    text: "false",
+                    at: 24,
+                    type: ETokenType.keyword,
+                    modifiers: [],
+                },
+                31: {
+                    text: "and",
+                    at: 31,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                35: {
+                    text: "true",
+                    at: 35,
+                    type: ETokenType.keyword,
+                    modifiers: [],
+                },
+            });
+        });
+
+        it("should set semantic tokens for unary operators", () => {
+            const expression = "not $varbl && def _var1 && ndef $var2";
+            const offset = 13;
+            const textDocument = TextDocument.create(
+                "fake-uri",
+                "twine",
+                1,
+                `twinescript: ${expression}`
+            );
+            const storyState: StoryFormatParsingState = {
+                passageTokens: {},
+            };
+
+            uut.tokenizeTwineScriptExpression(
+                expression,
+                offset,
+                textDocument,
+                storyState
+            );
+            const result = storyState.passageTokens;
+
+            expect(result).to.eql({
+                13: {
+                    text: "not",
+                    at: 13,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                18: {
+                    text: "varbl",
+                    at: 18,
+                    type: ETokenType.variable,
+                    modifiers: [],
+                },
+                24: {
+                    text: "&&",
+                    at: 24,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                27: {
+                    text: "def",
+                    at: 27,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                32: {
+                    text: "var1",
+                    at: 32,
+                    type: ETokenType.variable,
+                    modifiers: [],
+                },
+                37: {
+                    text: "&&",
+                    at: 37,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                40: {
+                    text: "ndef",
+                    at: 40,
+                    type: ETokenType.operator,
+                    modifiers: [],
+                },
+                46: {
+                    text: "var2",
+                    at: 46,
+                    type: ETokenType.variable,
+                    modifiers: [],
+                },
+            });
+        });
+    });
+});
